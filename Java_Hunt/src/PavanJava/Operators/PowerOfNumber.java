f0a482d377e52dc6779eebb5d6c6f5d0a1be532b
Sri Balaji V <sribalajihema2003@gmail.com>
2025-10-12 09:22:18 +0530

    PowerNumber added

--- Java_Hunt/src/PavanJava/Operators/PowerOfNumber.java
+++ Java_Hunt/src/PavanJava/Operators/PowerOfNumber.java
@@ -1,4 +1,24 @@
 package PavanJava.Operators;
 
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class PowerOfNumber {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        System.out.print("Enter Base: ");
+        int base = s.nextInt();
+
+        System.out.print("Enter Exponent: ");
+        int exponent = s.nextInt();
+
+        int result =1;
+
+        for (int i = 1; i <= exponent; i++) {
+            result *= base;
+        }
+
+        System.out.println(base + "^" +exponent+" = "+ result);
+    }
 }
