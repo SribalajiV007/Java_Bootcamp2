d23b6585f71300a7ff53ef3143ebe6eb5fb98001
Sri Balaji V <sribalajihema2003@gmail.com>
2025-10-12 09:39:16 +0530

    student report porject added

---
+++ Java_Hunt/src/PavanJava/MiniProject/StudentMain.java
@@ -0,0 +1,8 @@
+package PavanJava.MiniProject;
+
+public class StudentMain {
+    public static void main(String[] args) {
+        Student s1 = new Student("Sri Balaji",101177,94,98,97);
+        s1.displayReport();
+    }
+}
